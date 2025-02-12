6d809d638401a37611475f73a2c7a6542e8df0bb
Harshit chauhan <kcn9596@gmail.com>
2025-02-12 21:43:08 +0530

    fixed bug

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -64,9 +64,9 @@ export default function ContactPage() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     setIsSubmitting(true)
-    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+    // const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    // const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    // const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
 
     try {
       if (form.current) {
